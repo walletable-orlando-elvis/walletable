5ca9093fced04162ed7fbb27f8d1db4c7f09431a
ElvisQG <elvis.sqg@gmail.com>
2020-07-17 20:59:23 -0500

    Initial settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Formik, Field, Form, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
 import './App.css';
+
+
+function Label(props) {
+  return <label style={{ display: "block" }} {...props} />;
+}
+ 
+function FullField({ name, label }) {
+  return (
+    <div>
+      <Label name="fvfgvtbtbh">{label}</Label>
+      <Field name={name} />
+      <div>
+        <ErrorMessage name={name} />
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik
+      initialValues={{
+        email: "",
+        password: ""
+      }}
+      validationSchema={Yup.object({
+        email: Yup.string()
+          .email()
+          .required(),
+        password: Yup.string()
+          .min(6, "Must have more than 6 digits")
+          .required(),
+      })}
+      onSubmit={(values, actions) => {
+        // api function
+        console.log({ values, actions });
+      }}
+    >
+      <Form>
+        <h2>Login</h2>
+        <FullField name="email" label="Email" />
+        <FullField name="password" label="Password" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 }
 
